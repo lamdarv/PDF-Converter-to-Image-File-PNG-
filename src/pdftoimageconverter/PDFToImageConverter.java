d4dbb442885ab1fd30b77560f0aba883982d362f
lamdarv <78491230+lamdarv@users.noreply.github.com>
2022-03-20 01:56:23 +0700

    beres

---
+++ src/pdftoimageconverter/PDFToImageConverter.java
@@ -0,0 +1,28 @@
+package pdftoimageconverter;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.util.ImageIOUtil;
+
+public class PDFToImageConverter {
+	public static void main(String[] args) throws IOException {
+		
+		String pdfFileName = "percobaan.pdf";
+		PDDocument document = PDDocument.loadNonSeq(new File(pdfFileName), null);
+		
+		List<PDPage> pdPages = document.getDocumentCatalog().getAllPages();
+		
+		int page = 0;
+		
+		for(PDPage pdPage : pdPages) {
+			++page;
+			BufferedImage bim = pdPage.convertToImage(BufferedImage.TYPE_INT_RGB, 300);
+			ImageIOUtil.writeImage(bim, pdfFileName+"-"+page+".png", 300);
+		}
+	}
+}
